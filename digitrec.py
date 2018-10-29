283d2aa594f70380d6bbc4b01d7c2161d174a3d6
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-10-29 16:26:35 +0000

    added sequential model

--- digitrec.py
+++ digitrec.py
@@ -3,6 +3,13 @@
 # learning algorithm and the MNIST dataset.
 
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
 
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+(data_training, label_training), (data_test, label_test) = tf.keras.datasets.mnist.load_data()
+
+
+image_index = 7777 # You may select anything up to 60,000
+print(label_training[image_index]) # The label is 8
+plt.imshow(data_training[image_index], cmap='Greys')
