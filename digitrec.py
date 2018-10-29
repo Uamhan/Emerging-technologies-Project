2b3169dc6795006ccc20d0131f76533f7a55638a
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-10-29 18:30:28 +0000

    added save functionaility so the model dosent have to be compiled every time the script is run

--- digitrec.py
+++ digitrec.py
@@ -17,30 +17,28 @@ import keras.models
 data_test = data_test.reshape(data_test.shape[0], 28, 28, 1)
 data_training = data_training.reshape(data_training.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
+try:
+    model = keras.models.load_model('MNIST_MODEL.h5')
+except IOError:
 
-#creating our sequential model. for our CNN(Convolutional neural network).
-#represents a linear stack of layers.
-model = Sequential()
-#adds a 2d convolutional layer to the model. with filter size of 28 and a kernal size of (3,3)
-model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
-#adds a pooling lauer with a pool size of (2,2)
-model.add(MaxPooling2D(pool_size=(2, 2)))
-# Flattens arrays to fully connected layers
-model.add(Flatten()) 
-#adds standard densley conected NN layer
-model.add(Dense(128, activation=tf.nn.relu))
-#add drop out rate of 0.2 to prevent overfitting.
-model.add(Dropout(0.2))
-#final layer of the model with an out put of 10 to represent the 10 possible classifications 0-9
-model.add(Dense(10,activation=tf.nn.softmax))
-#compiles our model for use with the optimizer adam. The sparse categorical crossentropy loss algorythm.
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=data_training,y=label_training, epochs=10)
+    #creating our sequential model. for our CNN(Convolutional neural network).
+    #represents a linear stack of layers.
+    model = Sequential()
+    #adds a 2d convolutional layer to the model. with filter size of 28 and a kernal size of (5,5)
+    model.add(Conv2D(28, kernel_size=(5,5), input_shape=input_shape))
+    #adds a pooling lauer with a pool size of (3,3)
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+    # Flattens arrays to fully connected layers
+    model.add(Flatten()) 
+    #adds standard densley conected NN layer
+    model.add(Dense(128, activation=tf.nn.relu))
+    #add drop out rate of 0.2 to prevent overfitting.
+    model.add(Dropout(0.4))
+    #final layer of the model with an out put of 10 to represent the 10 possible classifications 0-9
+    model.add(Dense(10,activation=tf.nn.softmax))
+    #compiles our model for use with the optimizer adam. The sparse categorical crossentropy loss algorythm.
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x=data_training,y=label_training, epochs=10)
+    model.save('MNIST_MODEL.h5')
 
 print(model.evaluate(data_test, label_test))
-
-model.save('MNIST_MODEL.h5')
-
-new_model = keras.models.load_model('MNIST_MODEL.h5')
-
-print(new_model.evaluate(data_test,label_test))
