db14c63fcb0d6213e7453df0bf99b0a2b63f4dea
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-10-29 14:58:18 +0000

    imported and setup mnist dataset

--- digitrec.py
+++ digitrec.py
@@ -1,3 +1,8 @@
 # a Python script that takes an image file
 # containing a handwritten digit and identifies the digit using a supervised
 # learning algorithm and the MNIST dataset.
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
