c8b27899b04ee4e717e39ea981fb398c388bbf6e
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-10-29 17:07:06 +0000

    added compile functionality for model and fit it to training data

--- digitrec.py
+++ digitrec.py
@@ -6,10 +6,34 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+from keras.models import Sequential
 
+#Importing the training and test data from the tensoflow keras library.
 (data_training, label_training), (data_test, label_test) = tf.keras.datasets.mnist.load_data()
 
+# changing array to 4-dimesions to fit Keras API
+data_test = data_test.reshape(data_test.shape[0], 28, 28, 1)
+data_training = data_training.reshape(data_training.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
 
-image_index = 7777 # You may select anything up to 60,000
-print(label_training[image_index]) # The label is 8
-plt.imshow(data_training[image_index], cmap='Greys')
+#creating our sequential model. for our CNN(Convolutional neural network).
+#represents a linear stack of layers.
+model = Sequential()
+#adds a 2d convolutional layer to the model. with filter size of 28 and a kernal size of (3,3)
+model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
+#adds a pooling lauer with a pool size of (2,2)
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Flattens arrays to fully connected layers
+model.add(Flatten()) 
+#adds standard densley conected NN layer
+model.add(Dense(128, activation=tf.nn.relu))
+#add drop out rate of 0.2 to prevent overfitting.
+model.add(Dropout(0.2))
+#final layer of the model with an out put of 10 to represent the 10 possible classifications 0-9
+model.add(Dense(10,activation=tf.nn.softmax))
+#compiles our model for use with the optimizer adam. The sparse categorical crossentropy loss algorythm.
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=data_training,y=label_training, epochs=10)
+
+#model.evaluate(data_test, label_test)
