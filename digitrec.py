05aff0c8205ee3a7d4c36908301fcf46768a9949
Uamhan <31971768+Uamhan@users.noreply.github.com>
2018-11-26 01:55:57 +0000

    changed keras model to get more accurate results and redefined code iinto a method that takes in user image

--- digitrec.py
+++ digitrec.py
@@ -10,47 +10,61 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
 import keras.models
 
-#Importing the training and test data from the tensoflow keras library.
-(data_training, label_training), (data_test, label_test) = tf.keras.datasets.mnist.load_data()
 
-# changing array to 4-dimesions to fit Keras API
-data_test = data_test.reshape(data_test.shape[0], 28, 28, 1)
-data_training = data_training.reshape(data_training.shape[0], 28, 28, 1)
-input_shape = (28, 28, 1)
 
-#trys to load the a pre compiled model if not found in the current directory creates a new model and fits the dataset to it.
-try:
-    model = keras.models.load_model('MNIST_MODEL.h5')
-except:
-    #creating our sequential model. for our CNN(Convolutional neural network).
-    #represents a linear stack of layers.
-    model = Sequential()
-    #adds a 2d convolutional layer to the model. with filter size of 28 and a kernal size of (5,5)
-    model.add(Conv2D(28, kernel_size=(5,5), input_shape=input_shape))
-    #adds a pooling lauer with a pool size of (3,3)
-    model.add(MaxPooling2D(pool_size=(3, 3)))
-    # Flattens arrays to fully connected layers
-    model.add(Flatten()) 
-    #adds standard densley conected NN layer
-    model.add(Dense(128, activation=tf.nn.relu))
-    #add drop out rate of 0.2 to prevent overfitting.
-    model.add(Dropout(0.4))
-    #final layer of the model with an out put of 10 to represent the 10 possible classifications 0-9
-    model.add(Dense(10,activation=tf.nn.softmax))
-    #compiles our model for use with the optimizer adam. The sparse categorical crossentropy loss algorythm.
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x=data_training,y=label_training, epochs=10)
-    #saves the model so that we only need to compile once.
-    model.save('MNIST_MODEL.h5')
 
-#randomly picks a number from 0 to 9999 for the imgindex
-imgIndex = np.random.randint(10000)
-#plots the test data from at index imgindex
-plt.imshow(data_test[imgIndex].reshape(28,28),cmap='Greys')
-#use the model created previously to predict what the number the img represents is
-prediction = model.predict(data_test[imgIndex].reshape(1, 28, 28,1))
-#priints the predicted value.
-print("predicted value = {}".format(prediction.argmax()))
-#shows the plot of the image.
-plt.show()
+def predict_image(userimage):
+    #Importing the training and test data from the tensoflow keras library.
+    (data_training, label_training), (data_test, label_test) = tf.keras.datasets.mnist.load_data()
+
+    # changing array to 4-dimesions to fit Keras API
+    data_test = data_test.reshape(data_test.shape[0], 28, 28, 1)
+    data_training = data_training.reshape(data_training.shape[0], 28, 28, 1)
+    input_shape = (28, 28, 1)
+
+    #trys to load the a pre compiled model if not found in the current directory creates a new model and fits the dataset to it.
+    try:
+        model = keras.models.load_model('MNIST_MODEL.h5')
+    except:
+        #sequential model for mnist predictions
+        model = Sequential()
+        #adds a convolutional layer with 32 nodes relu activation
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+        #adds a convolutional layer with 64 nodes relu activation
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        #maxpooling with a size of 2,2
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        #drop out layer with 0.25 ratio
+        model.add(Dropout(0.25))
+        #flattens the current layer
+        model.add(Flatten())
+        #ads a dense layer with 128 nodes with relu activation
+        model.add(Dense(128, activation='relu'))
+        #droup out layer with a ratio of 0.5
+        model.add(Dropout(0.5))
+        #final dense layer that will produce one of 10 results
+        model.add(Dense(10,activation=tf.nn.softmax))
+        #compiles the model
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        #fits the model to the training data
+        model.fit(data_training, label_training,epochs=10,validation_data=(data_test, label_test))
+        #saves the model weights to a file to prevent need to refit on subsequent uses
+        model.save('MNIST_MODEL.h5')
+
+    
+    #converts user inputed image
+    npImageArray = np.array(userimage)
+    #predicts the digit from user image using the model
+    prediction = model.predict(npImageArray.reshape(1,28,28,1))
+    #returns predicted value
+    return(prediction.argmax())
+    
+
+
+
+
+
+
+
+    
 
